1ae3a26eea36ebd541b051f884b97286b583f4e1
K Nitish Kumar Gupta <nitishg175@gmail.com>
2023-03-30 16:44:04 +0530

    Create binarySearchTree.py

---
+++ day4/binarySearchTree.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end =" ")
+        inorder(root.right)
+
+def minValueNode(node):
+    current = node
+    while(current.left is not None):
+        current = current.left
+    return current
+
+def deleteNode(root, key):
+    if root is None:
+        return root
+    if (key < root.key):
+        root.left = deleteNode(root.left, key)
+    elif(key > root.key):
+        root.right = deleteNode(root.right, key)
+    else:
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        temp = minValueNode(root.right)
+        root.key = temp.key
+        root.right = deleteNode(root.right, temp.key)
+    return root
+root = None
+root = insert(root,8)
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
+
+print("Inorder traversal : ",end=' ')
+inorder(root)
+
+print("\n deletion")
+root = deleteNode(root,10)
+print(" in order = ",end=' ')
+inorder(root)
